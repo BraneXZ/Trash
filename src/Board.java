1750b92925540389397b5a329578558f7192c1f1
Israel Perez <israpere@gmail.com>
2018-06-20 17:10:43 -0500

    Updated Board

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,19 @@
-
-public class Board {
-
+import java.awt.*;
+import javax.swing.*;
+public class Board extends JFrame {
+	JPanel panel;
 	public static void main(String[] args) {
 		System.out.println("Test");
+		Board b = new Board();
+		b.setVisible(true);
 	}
-
+	public Board()
+	{
+		setTitle("Trash");
+		setSize(400,400);
+		setResizable(false);
+		panel = new JPanel();
+		panel.setBackground(Color.BLUE);
+		add(panel);
+	}
 }
